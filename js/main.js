4c90158d871df318268f8fbeb9aa02b8d2f69c32
Dennis Zepeda <dennis.zepeda@hotmail.com>
2014-07-20 02:14:21 -0600

    Add HomeView Class

--- js/main.js
+++ js/main.js
@@ -6,25 +6,11 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
+    
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new WebSqlStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
         // this.store = new MemoryStore(function() {
         //     self.showAlert('Store Initialized', 'Info');
