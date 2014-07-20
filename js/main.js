742f5d4ffeb9b0dd6755a64ad2ddbcb2804e5003
Dennis Zepeda <dennis.zepeda@hotmail.com>
2014-07-20 00:37:22 -0600

    Alert

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 var app = {
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     findByName: function() {
         console.log('findByName');
@@ -14,9 +21,12 @@ var app = {
     },
 
     initialize: function() {
+        var self = this;
         // this.store = new WebSqlStore();
         // this.store = new MemoryStore();
-        this.store = new LocalStorageStore();
+        this.store = new LocalStorageStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
